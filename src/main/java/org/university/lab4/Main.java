673ff4f4ee83eeca7dd5ee9799bfcb5f23b0ae23
sasha <sashazemzer@gmail.com>
2018-11-26 11:05:56 +0300

    переделал алгоритм полностью

--- src/main/java/org/university/lab4/Main.java
+++ src/main/java/org/university/lab4/Main.java
@@ -3,17 +3,19 @@ package org.university.lab4;
 public class Main {
     public static void main(String[] args) {
         Randomizer randomizer = new Randomizer();
-        Service service = new Service();
-
+        Service service_ = new Service();
         for (int i = 0; i < 100; i++) {
-            Request request = new Request(randomizer.getNormalY(), "Клиент№" + (i + 1));
-            service.addToQueue(request);
+            double after;
+            if (i == 0) {
+                after = 0;
+            } else {
+                after = randomizer.getUniformY();
+            }
+            service_.addRequest(after, randomizer.getNormalY());
         }
-        System.out.println("-----------------------");
-        System.out.println("Приём заявок завершён");
-        double remaining = service.serviceRemaining();
-        System.out.printf("Оставшиеся клиенты обслужены. Потребовалось %4.2f минут\n", remaining);
-        System.out.println("Среднее время ожидания = " + service.averageRequestWait());
-        System.out.println("Среднее время простоя = " + service.averageServiceWait());
+
+        System.out.println("Среднее время ожидания = " + service_.getAvgRequestTime());
+        System.out.println("Среднее время простоя = " + service_.getAvgServiceTime());
+
     }
 }
--- src/main/java/org/university/lab4/Service.java
+++ src/main/java/org/university/lab4/Service.java
@@ -1,84 +1,29 @@
 package org.university.lab4;
 
-import java.util.ArrayDeque;
-
 public class Service {
-    private ArrayDeque<Request> queue = new ArrayDeque<>();
-    private Randomizer randomizer = new Randomizer();
-
-    private double requestWait = 0;
+    private double channel = 0;
+    private double reception = 0;
+    private double requestWaitTime = 0;
+    private double serviceWaitTime = 0;
     private int requestCount = 0;
-    private double serviceWait = 0;
 
-    public void addToQueue(Request request) {
-        System.out.println("-----------------------------");
-        double requestWait = 0;
-        if (!queue.isEmpty()) {
-            requestWait = randomizer.getUniformY();
-            System.out.printf("Прошло %5.2f минут\n", requestWait);
-            System.out.println("-----------------------------");
+    public void addRequest(double after, double serviceTime) {
+        reception += after;
+        if (channel <= reception) {
+            serviceWaitTime += (reception - channel);
+            channel = reception + serviceTime;
+        } else {
+            requestWaitTime += (channel - reception);
+            channel += serviceTime;
         }
-        System.out.println(request.getName() + " подал заявку");
-        service(requestWait);
-        queue.add(request);
-        addLastWait();
         requestCount++;
-        printQueueTimeLeft();
     }
 
-    private void addLastWait() {
-        double w = 0;
-        Request lastElement = queue.peekLast();
-        if (lastElement != null) {
-            for (Request r : queue) {
-                if (!r.equals(lastElement)) {
-                    w += r.getServiceTime();
-                }
-            }
-        }
-        requestWait += w;
+    public double getAvgRequestTime() {
+        return requestWaitTime / requestCount;
     }
 
-    private void service(double minutes) {
-        while (minutes > 0 && !queue.isEmpty()) {
-            Request first = queue.peek();
-            minutes -= first.getServiceTime();
-            if (minutes >= 0.0) {
-                Request remove = queue.remove();
-                System.out.println(remove.getName() + " обслужен");
-            } else {
-                first.setServiceTime(Math.abs(minutes));
-            }
-        }
-        if (minutes > 0) {
-            serviceWait += minutes;
-        }
-
+    public double getAvgServiceTime() {
+        return serviceWaitTime / requestCount;
     }
-
-    public double serviceRemaining() {
-        double minutes = 0;
-        for (Request request : queue) {
-            minutes += request.getServiceTime();
-            System.out.println(request.getName() + " обслужен");
-        }
-        queue.clear();
-        return minutes;
-    }
-
-    private void printQueueTimeLeft() {
-        for (Request request : queue) {
-            System.out.printf("\t%s осталось %4.2f времени для обслуживания\n", request.getName(), request.getServiceTime());
-        }
-    }
-
-    public double averageRequestWait() {
-        return requestWait / requestCount;
-    }
-
-    public double averageServiceWait() {
-        return serviceWait / requestCount;
-    }
-
-
 }
